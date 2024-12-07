3a2373d78c6aee3c64231351fa15891e74079fd7
Kyeongran <eggking2580@gmail.com>
2024-12-07 12:56:35 +0900

    :sparkles: feat : Add TodoRequestDto class with field initialization

---
+++ src/main/java/com/example/dto/TodoRequestDto.java
@@ -0,0 +1,15 @@
+package com.example.dto;
+
+import lombok.Getter;
+
+/**
+ * 클라이언트로부터 전달받은 요청 데이터
+ * 컨트롤러에서 사용되어 비즈니스 로직에 필요한 데이터 추출
+ * 요청 타입 = requestDTO
+ */
+@Getter
+public class TodoRequestDto {
+    private String task;
+    private String author;
+    private String pw;
+}
