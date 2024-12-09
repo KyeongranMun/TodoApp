1d3a1b4fb9ee4d4b35aed38bc2f416280e1ddb8d
Kyeongran <eggking2580@gmail.com>
2024-12-09 19:26:58 +0900

    :bulb: comments : Add comments in UpdateRequestDto class and DeleteRequestDto

--- src/main/java/com/example/dto/DeleteRequestDto.java
+++ src/main/java/com/example/dto/DeleteRequestDto.java
@@ -3,9 +3,10 @@ package com.example.dto;
 import lombok.Getter;
 
 /**
- * 일정 삭제 요청을 수행하는 DTO 객체
+ * DeleteRequestDto 클래스
+ * 클라이언트가 일정 삭제 요청을 할 때 필요한 데이터 결과를 반환하고 전송합니다.
  */
-@Getter
+@Getter // 모든 필드에 대한 Getter 메서드 자동 생성
 public class DeleteRequestDto {
     private String pw;
 }
